0b907e0e2259e3b9023d8eb9330a7f166dc83c0a
Muhammad Jawad Nasir <muhammad.j.nasir@nokia.com>
2025-09-10 01:23:18 +0200

    Playing

--- Sources/api-explorer/main.swift
+++ Sources/api-explorer/main.swift
@@ -1,6 +1,27 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
-
 import Foundation
 
-CLI.run()
+enum CLICommand: String {
+    case hello
+    case version
+}
+
+func runCommand(_ command: String) {
+    switch CLICommand(rawValue: command) {
+    case .hello:
+        print("👋 Hello from Swift API Explorer!")
+    case .version:
+        print("Swift API Explorer v0.1.0")
+    default:
+        print("Unknown command: \(command)")
+        print("Usage: swift run swift-api-explorer <hello|version>")
+    }
+}
+
+let arguments = CommandLine.arguments
+
+if arguments.count > 1 {
+    let command = arguments[1]
+    runCommand(command)
+} else {
+    print("Usage: swift run swift-api-explorer <hello|version>")
+}
